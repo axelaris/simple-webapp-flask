19166d3e5cf52004276e1d98961179ef8ed2aa96
Alexey Zakharov <alexey.zakharov@altoros.com>
2024-11-11 17:52:20 +0200

    Fetch from kv

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import os
 from flask import Flask
+import logging
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,7 +19,31 @@ def kv():
     KV_NAME = os.getenv('KV_NAME')
     VAULT_NAME = "https://"+KV_NAME+".vault.azure.net/"
     KEY_VAULT_SECRET_NAME = os.getenv('SECRET_NAME')
-    OUTPUT='KeyVault name is '+VAULT_NAME+", secret name is "+KEY_VAULT_SECRET_NAME
+
+    try:
+        if 'MSI_CLIENT_ID':
+            credential = DefaultAzureCredential(
+                managed_identity_client_id=os.getenv('MSI_CLIENT_ID')
+            )
+        else:
+            raise Exception
+    except Exception:
+        logger.error('Failed to obtain access token', exc_info=True)
+        raise Exception(
+            'Failed to obtain access token'
+        )
+
+    try:
+        secret_client = SecretClient(
+            vault_url=f"https://{VAULT_NAME}.vault.azure.net/", credential=credential)
+        secret = secret_client.get_secret(f"{KEY_VAULT_SECRET_NAME}")
+    except Exception:
+        logger.error('Failed to get secret', exc_info=True)
+        raise Exception(
+            'Failed to get secret'
+        )
+
+    OUTPUT='KeyVault name is '+VAULT_NAME+", secret name is "+KEY_VAULT_SECRET_NAME+", value is "+secret.value
 
     return OUTPUT
 
