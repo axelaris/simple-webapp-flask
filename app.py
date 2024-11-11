e361000271d85a10ca9c6e42c9ccab2a0e40b444
Alexey Zakharov <alexey.zakharov@altoros.com>
2024-11-11 17:37:10 +0200

    Added kv envs

--- app.py
+++ app.py
@@ -10,5 +10,14 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('/kv')
+def kv():
+    KV_NAME = os.getenv('KV_NAME')
+    VAULT_NAME = "https://"+KV_NAME+".vault.azure.net/"
+    KEY_VAULT_SECRET_NAME = os.getenv('SECRET_NAME')
+    OUTPUT='KeyVault name is '+VAULT_NAME+", secret name is "+KEY_VAULT_SECRET_NAME
+
+    return OUTPUT
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
